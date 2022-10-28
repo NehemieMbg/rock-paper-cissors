3c49187e7e0ad7663d1f5f54fc6e5f6fa725d73c
NehemieMbg <nehemie.mbg@gmail.com>
2022-10-28 20:45:56 +0200

    Game interface

--- script.js
+++ script.js
@@ -1,64 +1,122 @@
+let possibleChoices = document.querySelectorAll('button');
+const message = document.querySelector('.message');
+const rdResult = document.querySelector('.rd-result');
+const playerChoice = document.querySelector('.player-choice');
+const cptChoice = document.querySelector('.computer-choice');
+let usrScore = document.querySelector('.usr-score');
+let cptScore = document.querySelector('.cpt-score');
+
+let result;
+let computerChoice;
+let userSelection;
+let player = 0;
+let ai = 0;
+
+// Start the game a soon as the user click on a button
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+	userSelection = e.target.classList;
+
+	for (choice of userSelection) {
+		if (choice === 'rock') {
+			playerChoice.innerText = '👊';
+		} else if (choice === 'paper') {
+			playerChoice.innerText = '🖐';
+		} else if (choice === 'scissors') {
+			playerChoice.innerText = '✌️';
+		}
+	}
+
+	getComputerChoice();
+	playRound();
+	game();
+}))
+
+// Generates a random choice for the computer
 function getComputerChoice() {
+
 	let computer = Math.floor(Math.random() * 3)
 
 	if (computer === 0) {
+		cptChoice.innerText = '👊'
 		return "rock";
-	} else if (computer === 1) {
+	}
+	if (computer === 1) {
+		cptChoice.innerText = '🖐'
 		return "paper"
-	} else {
+	}
+	if (computer === 2) {
+		cptChoice.innerText = '✌️'
 		return "scissors"
 	}
 }
 
-let pScore = 0;
-let cScore = 0;
-
+// Display the result of the current round
 function playRound(playerSelection, computerSelection) {
-	playerSelection = prompt().toLocaleLowerCase();
-	computerSelection = getComputerChoice();
 
-	if (playerSelection === computerSelection) {
-		console.log("It's a draw")
-	}
+	computerSelection = getComputerChoice();
+	playerSelection = userSelection.toString();
+
 	// Rock and Scissors
-	else if (playerSelection == "rock" && computerSelection == "scissors") {
-		console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-		return pScore += 1;
+	if (playerSelection === "rock" && computerSelection === "scissors") {
+		rdResult.innerText = 'You Win!';
+		message.innerText = `${playerSelection} beats ${computerSelection}.`;
+		usrScore.innerText = player += 1
 	}
-	else if (playerSelection == "scissors" && computerSelection == "rock") {
-		console.log(`You Lost! ${computerSelection} beats ${playerSelection}`)
-		return cScore += 1;
+	else if (playerSelection === "scissors" && computerSelection === "rock") {
+		rdResult.innerText = 'You Lose!';
+		message.innerText = `${computerSelection} beats ${playerSelection}.`;
+		cptScore.innerText = ai += 1
 	}
+
 	// Paper and Rock
-	else if (playerSelection == "paper" && computerSelection == "rock") {
-		console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-		return pScore += 1;
+	else if (playerSelection === "paper" && computerSelection === "rock") {
+		rdResult.innerText = 'You Win!';
+		message.innerText = `${playerSelection} beats ${computerSelection}.`;
+		usrScore.innerText = player += 1
 	}
-	else if (playerSelection == "rock" && computerSelection == "paper") {
-		console.log(`You Lost! ${computerSelection} beats ${playerSelection}`)
-		return cScore += 1;
+	else if (playerSelection === "rock" && computerSelection === "paper") {
+		rdResult.innerText = 'You Lose!';
+		message.innerText = `${computerSelection} beats ${playerSelection}.`;
+		cptScore.innerText = ai += 1
 	}
+
 	// Scissors and paper
-	else if (playerSelection == "scissors" && computerSelection == "paper") {
-		console.log(`You Won! ${playerSelection} beats ${computerSelection}`)
-		return pScore += 1;
+	else if (playerSelection === "scissors" && computerSelection === "paper") {
+		rdResult.innerText = 'You Win!';
+		message.innerText = `${playerSelection} beats ${computerSelection}.`;
+		usrScore.innerText = player += 1
 	}
-	else if (playerSelection == "paper" && computerSelection == "scissors") {
-		console.log(`You Lost! ${computerSelection} beats ${playerSelection}`)
-		return cScore += 1;
+	else if (playerSelection === "paper" && computerSelection === "scissors") {
+		rdResult.innerText = 'You Lose!';
+		message.innerText = `${computerSelection} beats ${playerSelection}.`;
+		cptScore.innerText = ai += 1
+	}
+
+	// Draw
+	else {
+		rdResult.innerText = "Tie!";
+		message.innerText = "nobody wins.";
+		return;
 	}
 }
 
+// Initialize to 0 at the end of the game
+// Resets the game
 function game() {
-	for (let round = 0; round < 5; round++) {
-		playRound()
-		console.log(`Your Score: ${pScore} Computer Score: ${cScore}`)
-	}
-	if (pScore > cScore) {
-		return "You have won!"
-	} else if (pScore < cScore) {
-		return "You have lost!"
-	} else {
-		return "It's a draw!"
+
+	if (ai === 5) {
+		rdResult.innerText = "You have lost!";
+		player = 0;
+		ai = 0;
+		cptScore.innerText = ai;
+		usrScore.innerText = player;
+		return;
+	} else if (player === 5) {
+		rdResult.innerText = "You have won!";
+		player = 0;
+		ai = 0;
+		cptScore.innerText = ai;
+		usrScore.innerText = player;
+		return;
 	}
 }
